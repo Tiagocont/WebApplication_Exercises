e51c90bec8747d637a4488cc2a9aaf3e43281bd0
Tiago Tavares <63767305+Tiagocont@users.noreply.github.com>
2023-05-23 11:31:58 -0300

    Add files via upload

--- Bootstrap/Exercicios/Ex1/js/main.js
+++ Bootstrap/Exercicios/Ex1/js/main.js
@@ -1,1 +1,21 @@
+function calc() {
+    var n1 = parseInt(document.getElementById("idn1").value);
+    var soma = 0;
 
+    if(n1%2==0){
+        soma = n1+ 5;
+    }
+    else{
+        soma = n1+ 8;
+    }
+
+    document.getElementById("idR").value = soma;
+    //5 par e 8 impar
+
+    
+}
+
+function limp(){
+    document.getElementById("idn1").value = "";
+    document.getElementById("idR").value = "";
+}
