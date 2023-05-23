c0431cafbe496e08d6477bb4970928bec349c14a
Tiago Tavares <63767305+Tiagocont@users.noreply.github.com>
2023-05-23 10:51:03 -0300

    Add files via upload

--- Bootstrap/Aula/js/main.js
+++ Bootstrap/Aula/js/main.js
@@ -1,1 +1,15 @@
+function calc() {
+    var n1 = parseInt(document.getElementById("idn1").value);
+    var n2 = parseInt(document.getElementById("idn2").value);
 
+    var soma = n1+ n2;
+
+    document.getElementById("idR").value = soma;
+    
+}
+
+function limp(){
+    document.getElementById("idn1").value = "";
+    document.getElementById("idn2").value = "";
+    document.getElementById("idR").value = "";
+}
